e26e4fdcfc8cc892d0ea568251d5ec05d18562d0
Aime78 <85676961+Aime78@users.noreply.github.com>
2023-07-07 10:14:58 +0200

    email service added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import logo from "./logo-white.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -19,16 +19,16 @@ import reactjs from "./tech/reactjs.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
+import threejs from "./tech/next.png";
 
 import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import shopify from "./company/amalitech.png";
+import starbucks from "./company/hundred-devs.png";
+import tesla from "./company/lasting-dynamics.png";
 
 import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import decentraland from "./decentraland.png";
+import evogym from "./evogym.png";
 
 export {
   logo,
@@ -57,6 +57,6 @@ export {
   starbucks,
   tesla,
   carrent,
-  jobit,
-  tripguide,
+  decentraland,
+  evogym,
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -15,13 +15,12 @@ import {
   git,
   figma,
   docker,
-  meta,
   starbucks,
   tesla,
   shopify,
   carrent,
-  jobit,
-  tripguide,
+  evogym,
+  decentraland,
   threejs,
 } from "../assets";
 
@@ -46,7 +45,7 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "Frontend Developer",
     icon: mobile,
   },
   {
@@ -54,7 +53,7 @@ const services = [
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Mobile Developer",
     icon: creator,
   },
 ];
@@ -116,55 +115,45 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
+    title: "Software Engineer",
+    company_name: "100Devs",
     icon: starbucks,
-    iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    iconBg: "#4f46e5",
+    date: "Mar 2022 - Nov 2022",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Collaborating closely with backend developers.",
+      "Transforming design concepts and wireframes into functional and visually captivating interfaces.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
   {
-    title: "React Native Developer",
-    company_name: "Tesla",
+    title: "Frontend Developer",
+    company_name: "Lasting Dynamics",
     icon: tesla,
     iconBg: "#E6DEDD",
-    date: "Jan 2021 - Feb 2022",
+    date: "Nov 2022 - Mar 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Developing and implementing user interface and experience for web and mobile applications with React and React Native, using industry best practices.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Testing and debugging to ensure seamless compatibility across various browsers and devices.",
+      "Optimizing frontend components for optimal performance and responsiveness.",
     ],
   },
   {
-    title: "Web Developer",
-    company_name: "Shopify",
+    title: "Software Developer",
+    company_name: "Amalitech",
     icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
     iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    date: "Mar 2023 - Present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Collaborate with designers to create and implement visually appealing user interfaces using React, Next, and Tailwind.",
+      "Work with server-side technologies such as Node, Python to build the logic and functionality behind web applications, includes handling data storage and retrieval.",
+      "Implement APIs for communication between the frontend and backend, and integrating with databases like PostgreSQL, MongoDB.",
+      "Ensure the security and performance of the entire application by implementing authentication and authorization mechanisms.",
+      "Conducting thorough testing, and optimizing code and database queries for efficiency.",
+      "Collaborate with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
     ],
   },
 ];
@@ -203,11 +192,11 @@ const projects = [
       "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
     tags: [
       {
-        name: "react",
+        name: "nextjs",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "restapi",
         color: "green-text-gradient",
       },
       {
@@ -216,49 +205,50 @@ const projects = [
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/aime78/car-rental",
   },
   {
-    name: "Job IT",
+    name: "Gym website",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      "Web application that enables users to become members of the gym, manage their fitness and health, view estimated training program available.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
+        name: "vite",
         color: "green-text-gradient",
       },
       {
-        name: "scss",
+        name: "tailwind",
         color: "pink-text-gradient",
       },
     ],
-    image: jobit,
-    source_code_link: "https://github.com/",
+    image: evogym,
+    source_code_link: "https://github.com/aime78/gym-website",
   },
   {
-    name: "Trip Guide",
+    name: "Decentralized social media application",
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      "A social media platform that allows users to connect with other users, and follow other users.",
     tags: [
       {
-        name: "nextjs",
+        name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "supabase",
+        name: "chakra ui",
         color: "green-text-gradient",
       },
       {
-        name: "css",
+        name: "lens protocol",
         color: "pink-text-gradient",
       },
     ],
-    image: tripguide,
-    source_code_link: "https://github.com/",
+    image: decentraland,
+    source_code_link:
+      "https://github.com/aime78/decentralized-social-media-app",
   },
 ];
 
